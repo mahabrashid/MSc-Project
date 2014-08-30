068cfef290198672d04bfe1e78e2b80c0131585d
mahabrashid <mabz@donotemail.me>
2014-08-30 03:15:15 +0100

    PersonalIdentificationNumber is generated based on a work out.

--- Application Development/Test Applications/AllServerSideApplications/src/uk/ac/rm950/nonPublicDataSerializationServer/Manager.java
+++ Application Development/Test Applications/AllServerSideApplications/src/uk/ac/rm950/nonPublicDataSerializationServer/Manager.java
@@ -1,6 +1,7 @@
 package uk.ac.rm950.nonPublicDataSerializationServer;
 
 import java.rmi.RemoteException;
+import java.util.Random;
 
 public class Manager extends Employee {
 	private String secretInfo = null;
@@ -11,8 +12,19 @@ public class Manager extends Employee {
 	}
 
 	public void generatePersonalIdentificationNumber(String dob) {
-		// do some logical calculation here with the dob
-		this.personalIdentificationNumber = 1234L;
+		if (dob.matches("\\d{2}[-/]\\d{2}[-/]\\d{4}")) {
+			dob = dob.substring(0, 2) + dob.substring(3, 5) + dob.substring(6);
+			String reverseDob = new StringBuilder(dob).reverse().toString();
+			try {
+				this.personalIdentificationNumber = Long.valueOf(reverseDob);
+				return;
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		// if regex unmatched or there's a NumberFormat exception, then generate 8-digit long number
+		this.personalIdentificationNumber = 10000000L + (long) (new Random()
+				.nextDouble() * (99999999 - 10000000));
 	}
 
 	void setSecretInfo(String secretInfo) { // only accessible from classes in the same package,
